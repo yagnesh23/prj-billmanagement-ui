9d03ac9097770422bb1e2467f35246101f002428
yagneshchudasasma23@gmail.com <Yagnesh23@>
2025-09-10 20:15:18 +0530

    did changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/prj-billmanagement-ui/",   // 👈 repo name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
